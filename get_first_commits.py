ae472e773f9381a285c3c8ffb6b107725e894de1
Rachel Krohn <rkrohn@nd.edu>
2018-05-21 18:10:57 -0400

    adding user id mapping files

--- get_first_commits.py
+++ get_first_commits.py
@@ -9,27 +9,6 @@ import io
 import unicodedata
 from collections import defaultdict
 
-#save some data structure to json file
-def save_json(data, filename):
-	with open(filename, 'w') as fp:
-		json.dump(data, fp, indent=4, sort_keys=False)
-		
-#load json to dictionary
-def load_json(filename):
-	if os.path.isfile(filename):
-		with open(filename) as fp:
-			data = json.load(fp)
-			return data
-	return False
-	
-#run bash command
-def run_bash(command, shell=False):
-	if shell:
-		process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-	else:
-		process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-	output, error = process.communicate()
-
 #--- MAIN EXECUTION BEGINS HERE---#	
 
 #read userid mappings from files
