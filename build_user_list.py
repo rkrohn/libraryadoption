d7da00b3b6ea7f93e817870083ede2174e17d0b0
Rachel Krohn <rkrohn@nd.edu>
2018-06-04 22:22:14 -0400

    working on script to build user lists, but probably not finished yet

--- build_user_list.py
+++ build_user_list.py
@@ -1,4 +1,4 @@
-#first pass at commit parsing - builds NEW user list and commits structure
+#first pass at commit parsing - builds (approximate) user list from raw commit data
 
 import os.path
 import subprocess
@@ -10,13 +10,21 @@ import file_utils as utils
 
 #--- MAIN EXECUTION BEGINS HERE---#	
 
-#parse list of commit import statements into more useable form:
-#	list of users across all repos, with id, name, and email (not perfect)
-#	list of commits, each with user id, imported libraries, and timestamp
-email_to_id = dict()
-name_to_id = dict()
+#first step in parsing commit data: creates list of users across all repos, with id, name, and email (not perfect)
+email_to_id = utils.load_json("data_files/email_to_userid.json")
+name_to_id = utils.load_json("data_files/name_to_userid.json")
+if email_to_id == False or name_to_id == False:
+	email_to_id = dict()
+	name_to_id = dict()
 
-next_id = 0	#keep index of next user id to use
+next_id = 0		#keep index of next user id to use
+#easy version, find highest id in existing and start one higher than that
+for k, v in email_to_id:
+	if v > next_id:
+		next_id = v + 1
+for k, v, in name_to_id:
+	if v > next_id:
+		next_id = v + 1
 file_idx = 0	#count files as finished
 
 mystery_commit_count = 0
@@ -27,12 +35,12 @@ mystery_repo_flag = False
 for filename in os.listdir('commit_data'):
 	#for line in f:
 	for line in io.open("commit_data/%s" % filename, encoding="ISO-8859-1"):
-		#commit line
+		#commit data line
 		if line.startswith("#######"):
 			#extract commit metadata
 			line = line.replace("#######", "")
 			commit = [x.strip() for x in line.split(',')]	#email, name, UTC
-			#check for diff lines that happen to start with commit flag
+			#check for diff lines that happen to start with commit flag (sad)
 			if len(commit) < 3:
 				continue
 			email = commit[0]	#email is first token
@@ -41,6 +49,7 @@ for filename in os.listdir('commit_data'):
 			name = ""
 			for token in commit[1:-1]:
 				name = name + token
+			#normalize out all the weird characters
 			name = unicodedata.normalize('NFD', name).encode('ascii', 'ignore')	
 
 			#mystery committer, just skip and move on
@@ -61,14 +70,14 @@ for filename in os.listdir('commit_data'):
 					name_to_id[name] = next_id
 				next_id = next_id + 1
 			#have one or the other (but not both), use id we know for both
+			#seen email but not name
+			elif have_email == True and have_name == False:
+				if name != "":
+					name_to_id[name] = email_to_id[email]			
 			#seen name but not email
 			elif have_name == True and have_email == False: 
 				if email != "":
 					email_to_id[email] = name_to_id[name]
-			#seen email but not name
-			elif have_email == True and have_name == False:
-				if name != "":
-					name_to_id[name] = email_to_id[email]
 			#have seen both name and email
 			else: #have_email = T and have_name = T
 				name_id = name_to_id[name]
@@ -76,10 +85,13 @@ for filename in os.listdir('commit_data'):
 				#if point to different ids, combine into a single user
 				if name_id != email_id:
 					email_to_id[email] = name_id
+
+	#keep count of repos with mystery commits
 	if mystery_repo_flag == True:
 		mystery_repo_count = mystery_repo_count + 1
 		mystery_repo_flag = False
 
+	#periodic progress prints
 	file_idx = file_idx + 1
 	if file_idx % 1000 == 0:
 		print "finished", file_idx, "files"
@@ -91,11 +103,11 @@ users = dict((v, k) for k, v in name_to_id.iteritems())
 users.update(dict((v, k) for k, v in email_to_id.iteritems()))
 print "found", len(users), "users"
 
-#mystery stats
+#mystery stats - total mystery commits, and number of repos affected
 print mystery_commit_count, "mystery commits (not necessarily import commits)"
 print mystery_repo_count, "repos affected"
 
 #save results	
-utils.save_json(name_to_id, "name_to_userid.json")
-utils.save_json(email_to_id, "email_to_userid.json")
+utils.save_json(name_to_id, "data_files/name_to_userid.json")
+utils.save_json(email_to_id, "data_files/email_to_userid.json")
 print "final user list saved to name_to_userid.json and email_to_userid.json"
