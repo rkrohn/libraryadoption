1ae288cfd11db07bd842c2a7736fdf2c33c8d20c
Rachel Krohn <rkrohn@nd.edu>
2018-06-10 20:14:46 -0400

    finally worked out the timestamp nonsense

--- sort_commits.py
+++ sort_commits.py
@@ -2,10 +2,11 @@
 #also chunk the commits by year, creating separate output files for each
 
 import json
-import datetime
+from datetime import datetime, timedelta
 import package_type
 import file_utils as utils
 import os.path
+from collections import defaultdict
 
 #given sorted data, write the data to a file
 def write_sorted_to_file(data, filename):
@@ -19,38 +20,58 @@ def write_sorted_to_file(data, filename):
 module_type = package_type.get_type()
 
 #read in the data
-data = utils.load_json('data_files/all_commits_%s_small.json' % module_type)
+print "reading in commit data..."
+data = utils.load_json('data_files/all_commits_%s.json' % module_type)
 if data == False:
 	print "commits file does not exist, exiting"
 	exit(0)
+print "read in", len(data), "commits"
 
 #sort events by time
+print "sorting commits..."
 ordered_commits = sorted(data, key=lambda k: k['time'])
 
 #save to a single mega-file
+print "saving sorted commits..."
 write_sorted_to_file(ordered_commits, "data_files/all_commits_%s_sorted.json" % module_type)
-
 print len(ordered_commits), "sorted commits saved to data_files/all_commits_%s_sorted.json" % module_type
-
-#split entire list by year, use dictionary with years as keys to store
-year_commits = dict()
 
 #create directory for commits by year files (if doesn't already exist)
 if os.path.isdir("data_files/all_commits_by_year") == False:
 	os.makedirs("data_files/all_commits_by_year")
 
-#get first and last year occurring in data
-first_year = datetime.datetime.fromtimestamp(ordered_commits[0]['time']).year
-last_year = datetime.datetime.fromtimestamp(ordered_commits[-1]['time']).year
+#get first year occurring in data
+first_year = datetime.fromtimestamp(ordered_commits[0]['time']).year
+print "earliest commit in", first_year
 
-#split list of commits by year, save each year to a separate file
-for year in range(first_year, last_year+1):
-	#get this year's commits
-	year_commits[year] = [commit for commit in ordered_commits if datetime.datetime.fromtimestamp(commit['time']).year == year]
+#get last year occuring in data - trickier, because there are some rogue overflows
+last_year = -1
+last_idx = -1
+while last_year == -1:
+	try:
+		date = datetime(1970, 1, 1) + timedelta(ordered_commits[last_idx]['time'])
+		last_year = date.year
+	except:
+		#move index back
+		last_idx -= 1
+print "latest commit in", last_year
 
-	#save commits to file
-	write_sorted_to_file(year_commits[year], 'data_files/all_commits_by_year/%s_commits_%s_sorted.json' % (year, module_type))
+#split entire list by year, use dictionary with years as keys to store
+year_commits = defaultdict(list)
 
+#split list of commits by year
+for commit in data:
+	try:
+		year = datetime.fromtimestamp(commit['time']).year
+	except:
+		year = "overflow"
+	year_commits[year].append(commit)
+	
+#save each year to a separate file
+for year in year_commits:
 	print "   ", year, ":", len(year_commits[year]), "commits"
+	#save commits to file if list not empty
+	if len(year_commits[year]) != 0:
+		write_sorted_to_file(year_commits[year], 'data_files/all_commits_by_year/%s_commits_%s_sorted.json' % (year, module_type))
 
 print "year-divided commits saved to data_files/all_commits_by_year/<year>_commits_%s_sorted.json"  % module_type
