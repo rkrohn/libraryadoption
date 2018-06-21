9d1d85930ba353f3316a7cafc48f5a7fd8798375
Rachel Krohn <rkrohn@nd.edu>
2018-06-21 15:25:48 -0400

    prediction framework added

--- classify.py
+++ classify.py
@@ -10,9 +10,12 @@ def load_pickle(filename):
 #end load_pickle
 
 #load all data for specified year range (inclusive) and return as a single events list and separate labels list
-def load_year_range(start, end):
+def load_year_range(start, end=-1):
 	all_events = []
 	all_labels = []
+
+	if end == -1:
+		end = start
 
 	for year in range(start, end+1):
 		print("Loading events for", year)
@@ -30,12 +33,17 @@ def load_year_range(start, end):
 	return all_events, all_labels
 #end load_year_range
 
+#given an np array, replace any nan values with value
+def replace_nan(data, value = -1):
+	data[np.isnan(data)] = value
+#end replace_nan
+
 #--- MAIN EXECUTION BEGINS HERE---#
 
 #set training and testing years here
 training_start = 1993
 training_end = 1993		#this year will be included in training
-testing = 1994			#single year for testing
+testing_year = 1994		#single year for testing
 
 #load all training data
 training_events_raw, training_labels_raw = load_year_range(training_start, training_end)
@@ -43,19 +51,35 @@ training_events_raw, training_labels_raw = load_year_range(training_start, train
 #convert training data to np arrays
 training_events = np.asarray(training_events_raw, dtype=np.float32)
 training_labels = np.asarray(training_labels_raw, dtype=np.float32)
-print("read", training_events.shape[0], "training events with", training_events.shape[1], "features")
+print("read", training_events.shape[0], "training events with", training_events.shape[1], "features\n")
 
 #replace any nan values with -1
-training_events[np.isnan(training_events)] = -1
+replace_nan(training_events)
 
 #train the classifier
 clf = linear_model.SGDClassifier(shuffle=True)
-clf.fit(training_events, training_labels)
+print(clf.fit(training_events, training_labels))
 
-print("coefficients:", clf.coef_)
-print("intercept:", clf.intercept_)
+print("\ncoefficients:", clf.coef_)
+print("intercept:", clf.intercept_, "\n")
 
 #read testing data
+testing_events_raw, testing_labels_raw = load_year_range(testing_year)
 
+#convert testing data to np array
+testing_events = np.asarray(testing_events_raw, dtype=np.float32)
+testing_labels = np.asarray(testing_labels_raw, dtype=np.float32)
+print("read", testing_events.shape[0], "testing events with", testing_events.shape[1], "features")
+
+#replace any nan values with -1
+replace_nan(testing_events)
+
+#predict on the testing events
+predicted_labels = clf.predict(testing_events)
+
+#for now, just print predicted vs actual for the first 25 events
+print("\nactual\tpredicted")
+for i in range(0, 25):
+	print(testing_labels[0], "\t", predicted_labels[0])
 
 
