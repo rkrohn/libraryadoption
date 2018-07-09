0ac99f99c434f2d6270bc2b724e052fa10b7d468
Rachel Krohn <rkrohn@nd.edu>
2018-07-09 11:52:48 -0400

    add some columns to combined results for easier processing

--- combine_results.py
+++ combine_results.py
@@ -1,5 +1,49 @@
 import glob
 import sys
+import pandas as pd
+import os
+
+def label_training_period(row):
+	#entire years?
+	if row['training_month_first'] == 1 and row['training_month_last'] == 12:
+		#one year?
+		if row['training_month_first'] == 1 and row['training_month_last'] == 12:
+			return "one year"
+		#two years?
+		if row['training_year_last'] - row['training_year_first'] == 1:
+			return "two years"
+		#build label from year range
+		return str(row['training_year_first']) + '-' + str(row['training_year_last'])
+	#single year?
+	if row['training_year_first'] == row['training_year_last']:
+		#build label as number of months		
+		if row['training_month_first'] == 7 and row['training_month_last'] == 12:
+			return "six months"
+		if row['training_month_first'] == 12 and row['training_month_last'] == 12:
+			return "one month"
+	return "UNDEFINED"
+#end label_training_period
+
+def label_testing_period(row):
+	#entire years?
+	if row['testing_month_first'] == 1 and row['testing_month_last'] == 12:
+		return "one year"
+	if row['testing_month_first'] == row['testing_month_last']:
+		return "one month"
+	return "UNDEFINED"
+#end label_testing_period
+
+def label_features_removed(row):
+	if row['num_features'] == 33:
+		return "none"
+	if row['num_features'] == 31:
+		return "binary"
+	if row['num_features'] == 29:
+		return "StackOverflow"
+	if row['num_features'] == 27:
+		return "binary+SO"
+	return "UNDEFINED"
+#end label_features_removed
 
 #--- MAIN EXECUTION BEGINS HERE---#
 
@@ -8,6 +52,10 @@ if len(sys.argv) < 2:
 	print("Requires command line argument for results filename. Exiting")
 	exit(0)
 filename = sys.argv[1]
+
+#delete file if already exists (replace it)
+if os.path.exists(filename):
+	os.remove(filename)
 
 #get list of files to combine
 files = glob.glob('predict_tests/results*')
@@ -29,5 +77,23 @@ for idx in range(1, len(files)):
 
 #close output file
 fout.close()
-print("All data written to", filename)
 
+#all compiled, let's improve the format and add a few columns
+data = pd.read_csv(filename)
+
+#remove leading/trailing spaces of column headers
+data.columns = data.columns.map(lambda x: x.strip())
+
+#same thing for all the data
+for column in ['penalty', 'loss', 'fit_intercept', 'shuffle']:
+	data[column] = data[column].map(lambda x: x.strip())
+
+#add some columns for easier sorting/graphing: training period, testing period, features removed
+data.insert(5, "training_period", data.apply(label_training_period, axis=1))
+data.insert(9, "testing_period", data.apply(label_testing_period, axis=1))
+data.insert(11, "features_removed", data.apply(label_features_removed, axis=1))
+
+#save modified file
+data.to_csv(filename, index=False)
+
+print("All data written to", filename)
