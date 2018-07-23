7e7f791366da41b5d5f8de3549457ad372d00653
Rachel Krohn <rkrohn@nd.edu>
2018-07-23 15:37:16 -0400

    more counts

--- event_types_count.py
+++ event_types_count.py
@@ -27,8 +27,26 @@ with open(pik, "rb") as f:
 	rows = np.where((data[:,22]==0) & (data[:,20]==1))
 	print(len(rows[0]), "first-time usages")
 
+	#repeated usages
+	rows = np.where((data[:,22]==1) & (data[:,20]==1))
+	print(len(rows[0]), "repeat usages")
+
+	#how many potential adoptions (actually committed or not)
+	rows = np.where(data[:,22]==0)
+	print(len(rows[0]), "potential adoptions (including actual adoptions and all first-time uses)")
+
+	#count "missed" adoptions - could have been first usage, but wasn't committed
+	#user used package must be false (user not used package before) - idx 22 must be 0
+	#user added package must be false (user did NOT commit this package) -  idx 20 must be 0
+	rows = np.where((data[:,22]==0) & (data[:,20]==0))
+	print(len(rows[0]), "missed adoptions")
+
+	#last negative category: used before, but not committed again
+	rows = np.where((data[:,22]==1) & (data[:,20]==0))
+	print(len(rows[0]), "updated and not committed after previous usage")
+
 	#negative vs positive samples
 	print(sum(data[:,20]), "addition events")
-	print(len(data) - sum(data[:,20]), "skipped adoptions (anti-add)")
+	print(len(data) - sum(data[:,20]), "non-additions")
 
 
