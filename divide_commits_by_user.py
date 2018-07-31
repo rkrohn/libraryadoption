6d8f99972995e366df29697d485b46a5822526bf
Rachel Krohn <rkrohn@nd.edu>
2018-07-31 15:20:44 -0400

    simultaneous streaming of raw commit data and event data working:

--- divide_commits_by_user.py
+++ divide_commits_by_user.py
@@ -2,6 +2,7 @@ import json
 import os.path
 from collections import defaultdict
 import pickle
+from datetime import datetime
 
 #stream json data one object at a time (generator function)
 def stream(f):
@@ -28,6 +29,13 @@ def stream(f):
 			c = f.read(1) 	#eat comma between objects
 #end stream
 
+#given a filepath, load pickled data
+def load_pickle(filename):
+	with open(filename, "rb") as f:
+		data = pickle.load(f)
+	return data
+#end load_pickle
+
 #dump all currently cached data (for a single month) to file
 def dump_dict(chunk, name):
 
@@ -39,11 +47,30 @@ def dump_dict(chunk, name):
 	print ("saved %s users for %s user-ids" % (len(chunk), name))
 #end dump_dict
 
+#return pre-computed feature vector one at a time (generator function)
+def next_feature(year, month):
+	events = load_pickle("data_files/new_event_features/%s/%s_events.pkl" % (year, month))
+	for event in events:
+		yield event
+#end next_feature
+
+#return pre-computed event labels one at a time (generator function)
+def next_label(year, month):
+	labels = load_pickle("data_files/new_event_features/%s/%s_labels.pkl" % (year, month))
+	for label in labels:
+		yield label
+#end next_label
+
 
 #--- MAIN EXECUTION BEGINS HERE---#
 
 if __name__ == "__main__":
 	
+	data_month = -1
+	data_year = -1
+	event = [-1]
+	event_label = -1
+
 	#dictionary of dictionary of commits by user
 	#first key: user id / 1000
 	#second key: user id
@@ -54,9 +81,10 @@ if __name__ == "__main__":
 	if os.path.isdir("data_files/user_commits") == False:
 		os.makedirs("data_files/user_commits")
 
+	commit_count = 0
 
 	#stream data from sorted json files
-	for year in range(1993, 1994): #2018):		#read and process 1993 through 2018
+	for year in range(1990, 1995): #2018):		#read and process 1993 through 2018
 
 		print("Streaming", year)
 
@@ -64,8 +92,51 @@ if __name__ == "__main__":
 		f = open('data_files/all_commits_by_year/%s_commits_SUB_sorted.json' % year)
 		commits = stream(f)
 
-		#process all commits in date order
+		#process all commits in date order		
 		for c in commits:
+
+			#is this commit from a different month than the current feature data? if so, new feature file stream
+			date = datetime.fromtimestamp(c['time'])
+			if date.month != data_month or date.year != data_year:
+				print("  moving to", str(date.month)+"-"+str(date.year))
+				data_month = date.month
+				data_year = date.year
+				#init new generators for new month: features and labels
+				events = next_feature(data_year, data_month)	
+				event_labels = next_label(data_year, data_month)
+
+			#build list of adopted libraries from this commit
+			#first, skip any event features/labels that are not part of this commit
+			while commit_count > event[0]:
+				try:
+					event = next(events)
+					event_label = next(event_labels)
+				except StopIteration:
+					#finished the current month, but no match for this commit - must be empty event
+					event = [-1]
+					event_label = -1	
+					break
+
+			#match up event features/labels to build list of adopted libraries
+			adopted = []
+			while commit_count == event[0]:			
+				if event_label == 1:
+					adopted.append(event[19])
+				if (event[20] == 1 and event[19] not in c['add_libs']) or event[1] != c['user'] or event[2] != c['repo']:
+					print("FAIL")
+					print(commit_count, c)
+					print(event, event_label)
+					exit(0)
+					
+				#get next event/label pair
+				try:
+					event = next(events)
+					event_label = next(event_labels)
+				except StopIteration:
+					#finished the current month, stop
+					event = [-1]
+					event_label = -1
+
 			#grab user from commit
 			if c['user'] == '':
 				user = 0
@@ -77,8 +148,12 @@ if __name__ == "__main__":
 
 			#add this commit to user's list
 			by_user[mod][user].append(c)
+
+			commit_count += 1
+
 			
 		f.close()
+	exit(0)
 
 	#save each "chunk" (defined by /1000 key) as a separate pickle
 	for key in by_user:
