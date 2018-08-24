2967a802670d44de4d392b271a5289aef1c87abe
Rachel Krohn <rkrohn@nd.edu>
2018-08-24 19:25:55 -0400

    add method to dump dictionary (or multiple parallel dictionaries) to csv file

--- file_utils.py
+++ file_utils.py
@@ -1,6 +1,7 @@
 import json
 import os.path
 import subprocess
+import numpy as np
 
 #save some data structure to json file
 def save_json(data, filename):
@@ -22,3 +23,42 @@ def run_bash(command, shell=False):
 	else:
 		process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
 	output, error = process.communicate()
+
+#dump dictionary(ies) data of key->value to csv file, sorted by key
+#if data is a list, each item is a dictionary, and all dictionaries have the same keys
+#also pass in a list of data headers, one per column
+def dump_dict_csv(data, headers, filename):
+	data_keys = []
+	data_vals = []
+
+	#if list of dictionaries, handle differently
+	if type(data) is list:
+		data_keys = list(sorted(data[0].keys()))	#grab list of sorted keys
+
+		#create 2D list of values
+		for key in data_keys:
+			row = []
+			for data_dict in data:
+				row.append(data_dict[key])
+			data_vals.append(row)
+
+		#combine into single np array
+		data_array = np.column_stack((data_keys, data_vals))
+
+	#one dictionary, easier
+	else:
+		#convert dict to lists
+		for key in sorted(data.keys()):
+			data_keys.append(key)
+			data_vals.append(data[key])
+
+		data_array = np.column_stack((data_keys, data_vals))
+
+	#add headers to top of array
+	data_array = np.vstack((headers, data_array))
+
+	#save data with headers to csv
+	np.savetxt(filename, data_array, delimiter=",", fmt="%s")
+#end dump_dict_csv
+
+
