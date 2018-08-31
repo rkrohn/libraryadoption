c06f2864afd56861cdb241ed3ee603307f0fed87
Rachel Krohn <rkrohn@nd.edu>
2018-08-31 09:37:20 -0400

    works better if you use the loop variable

--- run_classify.sh
+++ run_classify.sh
@@ -7,6 +7,6 @@ for features in UPLS CPLS CULS CUPS C U P L S; do
 	STR=""$features"_$(date +"%F_%T")"
 	echo $(date +"%F_%T") running $features
 
-	time python3 classify_by_day.py "predict_tests/results_by_day_$STR" CUPLS > "predict_tests/test_by_day_$STR.log"
+	time python3 classify_by_day.py "predict_tests/results_by_day_$STR" $features > "predict_tests/test_by_day_$STR.log"
 	sleep 30
 done 
