a8343d233f5c3a2c17b8f4cbced41b1c0912200f
Rachel Krohn <rkrohn@nd.edu>
2018-08-30 23:43:52 -0400

    script for queueing up runs

---
+++ run_classify.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#STR="$(date +"%F_%T")" && time python3 classify_by_day.py "predict_tests/results_by_day_$STR" CUPLS > "predict_tests/test_by_day_$STR.log" &
+
+#loop featuresets
+for features in UPLS CPLS CULS CUPS C U P L S; do
+	STR=""$features"_$(date +"%F_%T")"
+	echo $(date +"%F_%T") running $features
+
+	time python3 classify_by_day.py "predict_tests/results_by_day_$STR" CUPLS > "predict_tests/test_by_day_$STR.log"
+	sleep 30
+done 
